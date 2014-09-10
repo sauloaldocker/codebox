c399c831514cb67297ae53e6968561bd2b7a857f
Saulo Alves <sauloal@gmail.com>
2014-09-11 00:21:59 +0200

    lamp+node

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ docker stop codebox
 docker rm   codebox
 
 SRC=$HOME/data/docker
-SRC_HTML=$SRC/lamp/html
+SRC_HTML_LAMP=$SRC/lamp/html
+SRC_HTML_NODE=$SRC/node
 #SRC=/home/saulo/data/lamp/html
 
-docker run -d --name codebox -p 127.0.1.1:8003:8000 -v $SRC_HTML:/data jeromebaum/codebox run /data --open
+docker run -d --name codebox -p 127.0.1.1:8003:8000 -v $SRC_HTML_LAMP:/data/lamp -v $SRC_HTML_NODE:/data/node jeromebaum/codebox run /data --open
